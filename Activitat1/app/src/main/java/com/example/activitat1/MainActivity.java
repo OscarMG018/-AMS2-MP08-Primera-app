8f5cc12a3b2754974e46a329cbed3f15c2f76625
OscarMG018 <108469203+OscarMG018@users.noreply.github.com>
2024-09-18 20:54:18 +0200

    init

---
+++ Activitat1/app/src/main/java/com/example/activitat1/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.activitat1;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Hello Toast", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
